15bd4d85cbe7110dd9d87bfdbf0b33be9f488dd7
lukehayes <lukepurplehayes@gmail.com>
2026-02-09 15:11:58 +0000

    refactor: component files now start with capital letter

--- src/Tag.jsx
+++ src/Tag.jsx
@@ -2,17 +2,15 @@ import { useState } from 'preact/hooks'
 
 function Tag(props)
 {
-
 	const [color, setColor] = useState('#00FFFF');
 
 	return(
-		<div class='text-sm rouned-md flex flex-col ' style='background-color: {color}; ' >
+		<div class='flex flex-col ' style='background-color: {color}; ' >
 		<p class='p-4 flex flex-row text-normal' style='background-color: {{ color }};'>{props.text}</p>
 
 		<button onClick={() => setColor((color) => '#F00F0F')}>
 			color is {color}
 		</button>
-
 
 		</div>
 	)
--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
-import Tag from './tag'
-import Container from './container'
+import Tag from './Tag'
+import Container from './Container'
 import './app.css'
 
 export function App()
